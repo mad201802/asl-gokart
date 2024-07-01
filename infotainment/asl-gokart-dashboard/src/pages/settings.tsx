6db70ec18d6482bfc49d4ae17eb4be3ee29caea4
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-07-02 01:24:46 +0200

    feat: Add Slider and Dialog components for advanced settings dialog; Implemented screen brightness slider

--- infotainment/asl-gokart-dashboard/src/pages/settings.tsx
+++ infotainment/asl-gokart-dashboard/src/pages/settings.tsx
@@ -1,27 +1,61 @@
 import BatteryIndicator from "@/components/shared/battery-indicator";
 import DigitalClock from "@/components/shared/clock";
 import LabeledSwitch from "@/components/shared/labeled-switch";
+import DriveModeIndicator from "@/components/shared/drive-mode-indicator";
 import TabsSelector from "@/components/shared/tabs-selector";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button"
+import { Slider } from "@/components/ui/slider"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Separator } from "@/components/ui/separator";
+import React, { useContext, useState } from "react";
+import { DriveModeContext } from "@/contexts/drive-mode-context";
+import { AdvancedSettingsContext } from "@/contexts/advanced-settings-context";
 
 const SettingsPage = () => {
+
+  const [showBrightnessWarning, setShowBrightnessWarning] = useState(false);
+
+  const { driveMode, setDriveMode } = useContext(DriveModeContext);
+  const { screenBrightness, setScreenBrightness } = useContext(AdvancedSettingsContext);
+
+  let handleSliderChange = (value: number) => {
+    console.log(value);
+    setScreenBrightness(value);
+    if(value < 25) {
+      setShowBrightnessWarning(true);
+    } else {
+      setShowBrightnessWarning(false);
+    }
+  }
+
   return (
     <div className="w-full flex flex-col">
       <div className="flex flex-row items-center justify-between px-2 py-1">
         <DigitalClock />
+        <DriveModeIndicator />
         <BatteryIndicator />
       </div>
 
-
-      <div className="flex flex-row items-center justify-between px-2 py-1">
+      {/* ### "Tabelle" mit Rows ### */}
+      <div className="flex flex-row justify-between items-start px-2 py-1 pt-10">
         {/* ### 1. Einstellungsblock ### */}
-        <div className="flex flex-col items-left justify-center gap-y-2 pt-10 pl-7">
+        <div className="flex flex-col items-left justify-center gap-y-2 pl-7">
           <LabeledSwitch id="advanced-logging" label="Advanced Logging" defaultValue={false} />
           <LabeledSwitch id="airplane-mode" label="Airplane Mode" defaultValue={false} />
           <LabeledSwitch id="on-by-default" label="On by Default" defaultValue={true} />
         </div>
-
         {/* ### 2. Einstellungsblock ### */}
-        <div className="flex flex-col items-left justify-center gap-y-2 pt-10 pl-7">
+        <div className="flex flex-col items-left justify-center gap-y-2 ms-16">
           <TabsSelector 
             label="Drive Mode" 
             options={[
@@ -29,7 +63,35 @@ const SettingsPage = () => {
               {value: "comfort", label: "Comfort"}, 
               {value: "sport", label: "Sport"}]} 
             defaultValue="eco"  
+            onValueChange={setDriveMode}
               />
+          <div>
+              <Label htmlFor="avanced-settings" className="text-base mr-5">Advanced Settings</Label>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button>Configure</Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Advanced Settings</DialogTitle>
+                  </DialogHeader>
+                  <DialogDescription>
+                    <p>Here you can find advanced settings</p>
+                    <Separator className="mt-3"/>
+                    <div className="flex flex-row">
+                      <Label htmlFor="slider" className="text-base mr-5">Screen Brightness</Label>
+                      <Slider onValueChange={(v) => handleSliderChange(Number(v))}  defaultValue={[screenBrightness]} max={100} step={1} />                    
+                    </div>
+                    {showBrightnessWarning && <p className="text-red-500">Brightness might be too low for driving in bright daylight!</p>}
+                  </DialogDescription>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button>Close</Button>
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+          </div>
         </div>       
 
       </div>
