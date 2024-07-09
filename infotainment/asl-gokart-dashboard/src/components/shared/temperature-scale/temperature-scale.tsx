e8dcc2b03dbf0879eb796be9b83ae60ad2edccf1
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-07-09 23:31:58 +0200

    Moved styling to tailwind class names and removed `styles.css` stylesheet

--- infotainment/asl-gokart-dashboard/src/components/shared/temperature-scale/temperature-scale.tsx
+++ infotainment/asl-gokart-dashboard/src/components/shared/temperature-scale/temperature-scale.tsx
@@ -1,6 +1,5 @@
 // ProgressComponent.jsx
 import * as  Progress from '@radix-ui/react-progress';
-import './styles.css'; // Import the CSS file
 
 interface TemperatureScaleProps {
     currentTemp: number;
@@ -17,18 +16,19 @@ const TemperatureScale = ({ currentTemp, minTemp, maxTemp }: TemperatureScalePro
 
 
   return (
-    <Progress.Root className='temperature-scale-root'>
-                <div className='temperature-scale-inner'>
-
-        <div style={{ marginLeft: `${percentage}%` }} className='temperature-indicator-container'>
-
-            <div
-                className='temperature-scale-indicator'
-            />
-            <div 
-                className='gradient'
-            />
-        </div>
+    <Progress.Root className='rounded-lg px-2 w-full h-4 relative z-0 overflow-visible bg-gradient-to-r from-blue-500 via-zinc-200 to-red-500'>
+        {/*  */}
+        <div className='flex w-full h-full'>
+            {/*  */}
+            <div style={{ marginLeft: `${percentage}%` }} className='temperature-indicator-container flex justify-center items-center relative w-5 h-full -left-1 ease-in-out z-0'>
+                <div
+                    className='absolute w-1 h-4 bg-black rounded-lg z-20 ease-in-out'
+                />
+                {/* Gradient */}
+                <div 
+                    className='absolute w-5 h-full z-10 bg-gradient-to-r from-transparent via-white to-transparent'
+                />
+            </div>
         </div>
 
 
