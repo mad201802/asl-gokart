75933b4a641598f9a5bb4987ccf222e1fd8b1c8b
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-07-22 18:16:20 +0200

    Converted label to a prop

--- infotainment/asl-gokart-dashboard/src/components/shared/temperature-scale/temperature-box.tsx
+++ infotainment/asl-gokart-dashboard/src/components/shared/temperature-scale/temperature-box.tsx
@@ -1,16 +1,17 @@
 import TemperatureScale from "./temperature-scale";
 
 interface TemperatureBoxProps {
+    label: string;
     maxTemp: number;
     minTemp: number;
     currentTemp: number;
 }
 
-const TemperatureBox = ({ currentTemp, minTemp, maxTemp }: TemperatureBoxProps) => {
+const TemperatureBox = ({ label, currentTemp, minTemp, maxTemp }: TemperatureBoxProps) => {
     return (
         <div className="flex flex-row items-center justify-center">
-            <div className="flex flex-col w-1/2 items-center justify-center p-1 border-2 border-black overflow-hidden">
-                <p>Battery</p>
+            <div className="flex flex-col w-1/2 items-center justify-center p-1 overflow-hidden">
+                <p>{label}</p>
                 {/* <Progress value={50} /> */}
                 <TemperatureScale currentTemp={currentTemp} minTemp={minTemp} maxTemp={maxTemp} />
             </div>
