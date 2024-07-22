0e947f33877690528d16d3de8ee7bba76974fc57
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-07-22 18:16:38 +0200

    added more temperature boxes

--- infotainment/asl-gokart-dashboard/src/pages/car.tsx
+++ infotainment/asl-gokart-dashboard/src/pages/car.tsx
@@ -15,7 +15,15 @@ const CarPage = () => {
                 <BatteryIndicator />
                 </div>
             </div>
-            <TemperatureBox currentTemp={50} minTemp={0} maxTemp={100}></TemperatureBox>
+
+            <div className="flex flex-col gap-y-4">
+            <TemperatureBox label="Battery" currentTemp={50} minTemp={0} maxTemp={100}></TemperatureBox>
+            <TemperatureBox label="Motor" currentTemp={33} minTemp={0} maxTemp={100}></TemperatureBox>
+            <TemperatureBox label="Controller 1" currentTemp={21} minTemp={0} maxTemp={100}></TemperatureBox>
+            <TemperatureBox label="Controller 2" currentTemp={24} minTemp={0} maxTemp={100}></TemperatureBox>
+            </div>
+
+
 
         </div>
 
