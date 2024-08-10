0d3deea1220366e8891eb293d71d5d30b7c9133c
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-08-10 03:56:52 +0200

    Added states for distance and speed limiting

--- infotainment/asl-gokart-dashboard/src/stores/motorSlice.ts
+++ infotainment/asl-gokart-dashboard/src/stores/motorSlice.ts
@@ -8,9 +8,17 @@ export interface MotorSlice {
     rpm: number
     rpmBoundaries: [number, number]
     speed: number
+    dailyDistance: number
+    speedLimit: number
+    maxSettableSpeed: number
+    minSettableSpeed: number
     setGear: (gear: Gears) => void
     setDriveMode: (driveMode: DriveModes) => void
     setThrottle: (throttle: number) => void
+    setDailyDistance: (dailyDistance: number) => void
+    setSpeedLimit: (speedLimit: number) => void
+    setMaxSettableSpeed: (maxSettableSpeed: number) => void
+    setMinSettableSpeed: (minSettableSpeed: number) => void
   }
   
 export const createMotorSlice: StateCreator<
@@ -25,7 +33,15 @@ export const createMotorSlice: StateCreator<
     rpm: 6000,
     rpmBoundaries: [0, 10000],
     speed: 0,
+    dailyDistance: 1234.5,
+    speedLimit: 35,
+    maxSettableSpeed: 35,
+    minSettableSpeed: 7,
     setGear: (gear: Gears) => set(() => ({ gear: gear })),
     setDriveMode: (driveMode: DriveModes) => set(() => ({ driveMode: driveMode })),
-    setThrottle: (throttle: number) => set(() => ({ throttle: throttle }))
+    setThrottle: (throttle: number) => set(() => ({ throttle: throttle })),
+    setDailyDistance: (dailyDistance: number) => set(() => ({ dailyDistance: dailyDistance })),
+    setSpeedLimit: (speedLimit: number) => set(() => ({ speedLimit: speedLimit })),
+    setMaxSettableSpeed: (maxSettableSpeed: number) => set(() => ({ maxSettableSpeed: maxSettableSpeed })),
+    setMinSettableSpeed: (minSettableSpeed: number) => set(() => ({ minSettableSpeed: minSettableSpeed })),
   })
