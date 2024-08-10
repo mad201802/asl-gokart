3362e1bef9b21270101fb4251cbb3b9360c65bf7
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-08-10 03:56:31 +0200

    Created shared slice for setter methods that manipulate states from multiple slices

--- infotainment/asl-gokart-dashboard/src/stores/useStore.ts
+++ infotainment/asl-gokart-dashboard/src/stores/useStore.ts
@@ -2,9 +2,11 @@ import { create } from 'zustand'
 import { MotorSlice, createMotorSlice } from './motorSlice'
 import { SystemSlice, createSystemSlice } from './systemSlice'
 import { BatterySlice, createBatterySlice } from './batterySlice'
+import { SharedSlice, createSharedSlice } from './sharedSlice'
 
-export const useStore = create<MotorSlice & SystemSlice & BatterySlice>()((...a) => ({
+export const useStore = create<MotorSlice & SystemSlice & BatterySlice & SharedSlice>()((...a) => ({
   ...createMotorSlice(...a),
   ...createSystemSlice(...a),
-  ...createBatterySlice(...a)
+  ...createBatterySlice(...a),
+  ...createSharedSlice(...a),
 }))
