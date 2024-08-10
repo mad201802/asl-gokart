f9a5b5f6650a504c6152160ebafc853c35af11a2
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-08-10 03:50:41 +0200

    added new states for additional battery information and state logic for calculating values; added UI to show these values;

--- infotainment/asl-gokart-dashboard/src/pages/battery.tsx
+++ infotainment/asl-gokart-dashboard/src/pages/battery.tsx
@@ -5,7 +5,8 @@ import { useStore } from "@/stores/useStore";
 
 const BatteryPage = () => {
 
-    const { batteryTemps, avgBatteryTemp, setBatteryTemps } = useStore();
+    const { batteryTemps, avgBatteryTemp, minTemp, maxTemp, voltage } = useStore();
+    const { setBatteryTemps } = useStore();
 
     // Generate 6 random battery temperatures from 10 to 40
     const randomBatteryTemps = () => {
@@ -28,8 +29,21 @@ const BatteryPage = () => {
                     minTemp={12.5} 
                     maxTemp={40}
                 />
-                <div className="text-black text-xl mt-4">Battery Temperature</div>
-                <div className="text-black text-xl">{avgBatteryTemp.toFixed(1)}</div>
+                <div className="flex flex-col mt-2 w-72">
+                    <div className="flex flex-row justify-between">
+                        <div className="text-black text-sm">Avg. Temperature</div>
+                        <div className="text-black text-sm">{avgBatteryTemp.toFixed(1)} °C</div>
+                    </div>
+                    <div className="flex flex-row justify-between">
+                        <div className="text-black text-sm">Min/Max Temperature</div>
+                        <div className="text-black text-sm">{minTemp.toFixed(1)} / {maxTemp.toFixed(1)} °C</div>
+                    </div>
+                    <div className="flex flex-row justify-between">
+                        <div className="text-black text-sm">Voltage</div>
+                        <div className="text-black text-sm">{voltage.toFixed(1)} V</div>
+                    </div>
+                </div>
+
                 <Button onClick={() => setBatteryTemps(randomBatteryTemps())}>Randomize Temps</Button>
             </div>
 
--- infotainment/asl-gokart-dashboard/src/stores/batterySlice.ts
+++ infotainment/asl-gokart-dashboard/src/stores/batterySlice.ts
@@ -3,9 +3,12 @@ import { StateCreator } from 'zustand'
 export interface BatterySlice {
     batteryTemps: number[],
     avgBatteryTemp: number,
+    minTemp: number,
+    maxTemp: number,
     batteryPercentage: number,
+    voltage: number,
     setBatteryTemps: (temps: number[]) => void,
-    setBatteryPercentage: (percentage: number) => void
+    setBatteryVoltage: (voltage: number) => void
   }
   
 export const createBatterySlice: StateCreator<
@@ -16,7 +19,27 @@ export const createBatterySlice: StateCreator<
   > = (set) => ({
     batteryTemps: [30, 35, 20, 25, 20, 20],
     avgBatteryTemp: 0,
+    minTemp: 0,
+    maxTemp: 0,
     batteryPercentage: 0.33,
-    setBatteryTemps: (temps: number[]) => set(() => ({ batteryTemps: temps, avgBatteryTemp: temps.reduce((a, b) => a + b, 0) / temps.length })),
-    setBatteryPercentage: (percentage: number) => set(() => ({ batteryPercentage: percentage }))
+    voltage: 67.2,
+    setBatteryTemps: (temps: number[]) => set(() => {
+      const avgBatteryTemp = temps.reduce((a, b) => a + b, 0) / temps.length;
+      const minTemp = Math.min(...temps);
+      const maxTemp = Math.max(...temps);
+      return {
+        batteryTemps: temps,
+        avgBatteryTemp,
+        minTemp,
+        maxTemp
+      };
+    }),
+    setBatteryVoltage: (voltage: number) => set(() => {
+      const minVoltage = 53;
+      const maxVoltage = 67.2;
+      const batteryPercentage = (voltage - minVoltage) / (maxVoltage - minVoltage);
+      return { 
+        voltage: voltage,
+        batteryPercentage: batteryPercentage };
+    }),
   })
