b87a09ce0b38f533a684de8c416218196240c303
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-08-30 02:04:28 +0200

    Added ws parameter to onMessage definition,;added zone to send definition;

--- infotainment/headunit/src/helpers/ipc/websocket/ws-context.ts
+++ infotainment/headunit/src/helpers/ipc/websocket/ws-context.ts
@@ -1,12 +1,18 @@
+import { IncomingZoneControllerMessage, OutgoingZoneControllerMessage } from '@/data/models';
 import { WEBSOCKET_MESSAGE_CHANNEL, WEBSOCKET_SEND_CHANNEL } from './ws-channels';
+import { WebSocket } from 'ws';
+import { Zones } from '@/data/controlling_models/zc';
 
 export function exposeWebSocketContext() {
     const { contextBridge, ipcRenderer } = window.require('electron');
 
     contextBridge.exposeInMainWorld('websocket', {
-        send: (message: string) => ipcRenderer.send(WEBSOCKET_SEND_CHANNEL, message),
-        onMessage: (callback: (message: string) => void) => {
-            ipcRenderer.on(WEBSOCKET_MESSAGE_CHANNEL, (_, message) => callback(message));
+        send: (message: OutgoingZoneControllerMessage, zone: Zones) => {
+            ipcRenderer.send(WEBSOCKET_SEND_CHANNEL, message, zone); 
+
+        },
+        onMessage: (callback: (message: IncomingZoneControllerMessage, ws: WebSocket) => void) => {
+            ipcRenderer.on(WEBSOCKET_MESSAGE_CHANNEL, (_, message, ws) => callback(message, ws));
         }
     });
 }
