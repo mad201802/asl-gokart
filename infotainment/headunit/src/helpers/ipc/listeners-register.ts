bac27e2fb471524b298b27c26d1453e7a3452f31
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-08-30 02:03:22 +0200

    Added functionality to send messages to ws clients using the WEBSOCKET_SEND_CHANNEL; Implemented filtering so only the right zone controller gets a message.

--- infotainment/headunit/src/helpers/ipc/listeners-register.ts
+++ infotainment/headunit/src/helpers/ipc/listeners-register.ts
@@ -2,21 +2,25 @@ import { BrowserWindow, ipcMain } from "electron";
 import { addThemeEventListeners } from "./theme/theme-listeners";
 import { addWindowEventListeners } from "./window/window-listeners";
 import { WEBSOCKET_SEND_CHANNEL } from "./websocket/ws-channels";
-import { startWebSocketServer } from "./websocket/ws-server";
+import { connected_zonecontrollers, startWebSocketServer, wss } from "./websocket/ws-server";
+import { OutgoingZoneControllerMessage } from "@/data/models";
+import { WebSocket } from "ws";
+import { Zones } from "@/data/controlling_models/main";
 
 export default function registerListeners(mainWindow: BrowserWindow) {
     addWindowEventListeners(mainWindow);
     addThemeEventListeners();
     startWebSocketServer(mainWindow);
 
-    // ipcMain.on(WEBSOCKET_SEND_CHANNEL, (event, message) => {
-    //     // Senden Sie die Nachricht an alle verbundenen WebSocket-Clients
-    //     if (wss) {
-    //         wss.clients.forEach((client) => {
-    //             if (client.readyState === client.OPEN) {
-    //                 client.send(message);
-    //             }
-    //         });
-    //     }
-    // });
+    ipcMain.on(WEBSOCKET_SEND_CHANNEL, (event, message: OutgoingZoneControllerMessage, zone: Zones) => {
+        // Send the message to the zone controller matching the specified zone
+        if (wss) {
+            connected_zonecontrollers.forEach((ws: WebSocket, key: string) => {
+                if (key === zone) {
+                    ws.send(JSON.stringify(message));
+                    console.log(`Sent message to [${key}] zone controller : ${JSON.stringify(message)}`);
+                }
+            });
+        }
+    });
 }
