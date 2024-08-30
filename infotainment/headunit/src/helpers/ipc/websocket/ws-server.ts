14619e905e34f1bdd6c2e5c8fb01706bcb79519f
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-08-30 02:05:20 +0200

    Implemented a map to keep track of connected zone controllers and their ws;

--- infotainment/headunit/src/helpers/ipc/websocket/ws-server.ts
+++ infotainment/headunit/src/helpers/ipc/websocket/ws-server.ts
@@ -1,20 +1,55 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 import { BrowserWindow } from 'electron';
+import { Zones } from 'src/data/controlling_models/zc';
 
 const WSS_PORT = 6969;
+export let wss: WebSocketServer;
+export let connected_zonecontrollers = new Map<string, WebSocket>();
 
 export function startWebSocketServer(mainWindow: BrowserWindow) {
-  const wss: WebSocketServer = new WebSocketServer({ port: WSS_PORT, host: '0.0.0.0' });
-  wss.on('connection', (ws) => {
+  wss = new WebSocketServer({ port: WSS_PORT, host: '0.0.0.0' });
+  wss.on('connection', (ws: WebSocket) => {
     console.log('WebSocket client connected');
 
     ws.on('message', (message) => {
       console.log(`Received message: ${message}`);
-      mainWindow.webContents.send('websocket-message', message.toString());
+      const zone = JSON.parse(message.toString()).zone;
+
+      // Check if the zone controller is already connected
+      if(!connected_zonecontrollers.has(zone)) {
+        
+        /* ######### THIS DOES NOT WORK ######### */
+        // Check if the zone controller is a valid zone
+//        if(Object.values(Zones).includes(zone)) {
+
+          // Add it to the map of connected zone controllers
+          connected_zonecontrollers.set(zone, ws);
+          console.log(`New zone controller connected: ${zone}`);
+          console.log(`Total zone controllers connected: ${connected_zonecontrollers.size}`);
+          console.log(Array.from(connected_zonecontrollers.keys()));
+        } else {
+          console.log(`Invalid zone controller [${zone}].`);
+          console.log('Closing connection...');
+          ws.close();
+        }
+//      } else {
+        // Send the message to the renderer process
+        mainWindow.webContents.send('websocket-message', message.toString());
+//      }
     });
 
     ws.on('close', () => {
       console.log('WebSocket client disconnected');
+      // Remove the zone controller from the map of connected zone controllers
+      connected_zonecontrollers.forEach((value, key) => {
+        if(value === ws) {
+          connected_zonecontrollers.delete(key);
+          console.log(`Zone controller ${key} disconnected`);
+          console.log(`Total zone controllers connected: ${connected_zonecontrollers.size}`);
+          console.log(Array.from(connected_zonecontrollers.keys()));
+        }
+      });
+
     });
   });
   console.log('WebSocket server is running on ws://localhost: ' + WSS_PORT);
