0acc84ab05d2fa8e68e21ed07e02a734c19d85c0
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-09-02 15:11:38 +0200

    implemented outgoing packet routing based on zones

--- infotainment/headunit/src/helpers/ipc/listeners-register.ts
+++ infotainment/headunit/src/helpers/ipc/listeners-register.ts
@@ -1,24 +1,24 @@
 import { BrowserWindow, ipcMain } from "electron";
 import { addThemeEventListeners } from "./theme/theme-listeners";
 import { addWindowEventListeners } from "./window/window-listeners";
-import { WEBSOCKET_SEND_CHANNEL } from "./websocket/ws-channels";
+import { WEBSOCKET_SEND_CHANNEL, WEBSOCKET_THROTTLE_MESSAGE_CHANNEL } from "./websocket/ws-channels";
 import { connected_zonecontrollers, startWebSocketServer, wss } from "./websocket/ws-server";
-import { OutgoingZoneControllerMessage } from "@/data/models";
+import { OutgoingPacket } from "@/data/zonecontrollers/packets";
 import { WebSocket } from "ws";
-import { Zones } from "@/data/controlling_models/main";
+import { ZoneController, Zones } from "@/data/zonecontrollers/zonecontrollers";
 
 export default function registerListeners(mainWindow: BrowserWindow) {
     addWindowEventListeners(mainWindow);
     addThemeEventListeners();
     startWebSocketServer(mainWindow);
 
-    ipcMain.on(WEBSOCKET_SEND_CHANNEL, (event, message: OutgoingZoneControllerMessage, zone: Zones) => {
+    ipcMain.on(WEBSOCKET_SEND_CHANNEL, (event, message: OutgoingPacket, zoneToSendTo: Zones) => {
         // Send the message to the zone controller matching the specified zone
         if (wss) {
-            connected_zonecontrollers.forEach((ws: WebSocket, key: string) => {
-                if (key === zone) {
-                    ws.send(JSON.stringify(message));
-                    console.log(`Sent message to [${key}] zone controller : ${JSON.stringify(message)}`);
+            connected_zonecontrollers.forEach((zc: ZoneController, z: Zones) => {
+                if (z === zoneToSendTo) {
+                    zc.webSocket.send(JSON.stringify(message));
+                    console.log(`[WEBSOCKET_SEND_CHANNEL] Sent message to [${z}] zone controller : ${JSON.stringify(message)}`);
                 }
             });
         }
