17369c3779d5b004c15ddd2b093a752f331eb263
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-09-16 15:25:07 +0200

    Changed implementation according to working prototype by Leo

--- zc/zc_battery/src/main.cpp
+++ zc/zc_battery/src/main.cpp
@@ -6,15 +6,49 @@
 
 // Olimex IP-Adresse unten im Code anpassen!
 // WICHITG: IP-Adresse und Port des WebSocket-Servers hier anpassen:
-#define WEB_SOCKET_SERVER_IP "192.168.1.100"
-#define WEB_SOCKET_SERVER_PORT 6969
+WebSocketsClient webSocket;             // WebSocket client instance
+
+const char* serverUrl = "192.168.1.100";  // WebSocket server address
+const int serverPort = 6969;              // WebSocket server port
+
+long lastTimeSent = 0;                 // Last time a message was sent
+long lastTimeSensorsSent = 0;
+int MESSAGE_INTERVAL = 5000;       // Interval between messages
+int MESSAGE_INTERVAL_SENSORS = 5000;
 
 float EXAMPLE_TEMP_ARRAY[] = { 20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0};
 
-WebSocketsClient webSocket;
 
-bool sendRegisterPacket();
+void sendRegister();
+void sendMsg();
+void sendSensorMsg();
 void onWebSocketEvent(WStype_t type, uint8_t *payload, size_t length);
+void WiFiEvent(WiFiEvent_t event);
+
+void WiFiEvent(WiFiEvent_t event) {
+  switch (event) {
+    case SYSTEM_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      // Set the hostname for the ESP32
+      ETH.setHostname("esp32-poe");
+      break;
+    case SYSTEM_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case SYSTEM_EVENT_ETH_GOT_IP:
+      Serial.print("ETH IP Address: ");
+      Serial.println(ETH.localIP());
+      break;
+    case SYSTEM_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      break;
+    case SYSTEM_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      break;
+    default:
+      break;
+  }
+}
 
 void onWebSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
     switch (type) {
@@ -23,6 +57,7 @@ void onWebSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
             break;
         case WStype_CONNECTED:
             Serial.printf("Connected to URL: %s\n", payload);
+            sendRegister();
             break;
         case WStype_TEXT:
             Serial.printf("Received text: %s\n", payload);
@@ -44,20 +79,26 @@ void onWebSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
 
 void setup() {
     Serial.begin(115200);
-    
+    delay(1000);
+    Serial.println("####################################");
+    Serial.println("ZC_BATTERY Fw. v.0.1.0");
+    Serial.println("");
+    Serial.println("Authors: AEROSPACE-LAB Team Gokart");
+    Serial.println("####################################");
     // Initialize Ethernet
+    WiFi.onEvent(WiFiEvent);
+    webSocket.onEvent(onWebSocketEvent);
     ETH.begin();
-    
     /* -------------------- HIER DIE IP-Adressen vom ESP und Gateway konfigurieren -------------------- */
-    ETH.config(IPAddress(192, 168, 1, 2), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
+    ETH.config(IPAddress(192, 168, 1, 4), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
     /* -------------------------------------------------------------------------------------------------*/
-    
     // Wait for Ethernet to connect
     Serial.print("Connecting to Ethernet");
     while (!ETH.linkUp()) {
         delay(500);
         Serial.print(".");
     }
+    // Print Ethernet information
     Serial.println("Connected to Ethernet");
     Serial.print("IP address: ");
     Serial.println(ETH.localIP());
@@ -66,22 +107,11 @@ void setup() {
     Serial.print("Subnet: ");
     Serial.println(ETH.subnetMask());
 
-    // Initialize WebSocket client
-    webSocket.begin(WEB_SOCKET_SERVER_IP, WEB_SOCKET_SERVER_PORT);
-    Serial.println("Connected to WebSocket server");
-    Serial.println("WebSocket server IP: " WEB_SOCKET_SERVER_IP);
-    Serial.print("WebSocket server port: ");
-    Serial.println(WEB_SOCKET_SERVER_PORT);
-    
-    webSocket.onEvent(onWebSocketEvent);
-    Serial.println("Attached event handler");
-    Serial.println("Trying to register this zone controller...");
-    while(!sendRegisterPacket()) {
-        Serial.println("Failed to send register packet. Retrying...");
-        delay(1000);
-    }
-    Serial.println("WebSocket client initialized");
+    // Connect to WebSocket server
+    webSocket.begin(serverUrl, serverPort, "/");
     delay(1000);
+    webSocket.loop();
+    Serial.println("Setup complete");
 
     // Initialize sensors
     initializeSensors();
@@ -90,15 +120,32 @@ void setup() {
 void loop() {
     // Maintain WebSocket connection
     webSocket.loop();
-    if(!webSocket.isConnected()) {
-        Serial.println("WebSocket disconnected. Trying to reconnect...");
-        webSocket.begin(WEB_SOCKET_SERVER_IP, WEB_SOCKET_SERVER_PORT);
-        while(!sendRegisterPacket()) {
-            Serial.println("Failed to send register packet. Retrying...");
-            delay(1000);
-        }
-    }
+    // Send messages to WebSocket server
+    // sendMsg();
+    sendSensorMsg();
+    delay(1);
+}
 
+
+void sendRegister() {
+  webSocket.sendTXT("{ \"zone\": \"battery\" }");
+  Serial.println("Register package sent");
+}
+
+void sendMsg() {
+    if (millis() - lastTimeSent < MESSAGE_INTERVAL) {
+        return;
+    }
+    Serial.println("Sending message to WebSocket server...");
+    // Send a message to the WebSocket server
+    webSocket.sendTXT("{ \"zone\": \"battery\", \"command\": \"getVoltage\", \"value\": 123.4 }");
+    lastTimeSent = millis();
+}
+
+void sendSensorMsg() {
+    if (millis() - lastTimeSensorsSent < MESSAGE_INTERVAL_SENSORS) {
+        return;
+    }
     // Get temperature data from sensors
     std::vector<float> temperatures = sensorLoop();
     Serial.println("Sensors array before sending: ");
@@ -117,7 +164,6 @@ void loop() {
         "value": [20.0, 25.0, 30.0, 35.0, 40.0, 45.0, 50.0, 55.0]
     }
     */
-   
     char output[256];
     StaticJsonDocument<256> doc;
     doc["zone"] = "battery";
@@ -138,39 +184,10 @@ void loop() {
         if(webSocket.sendTXT(output)) {
             Serial.println(F("Temperature data sent"));
             Serial.println(output);
+            lastTimeSensorsSent = millis();
         } else {
             Serial.println(F("Failed to send temperature data"));
+            lastTimeSensorsSent = millis();
         }
-    delay(500);
-    }
-}
-
-
-
-bool sendRegisterPacket() {
-    // Send register packet to WebSocket server
-    /*
-    Format of the JSON message:
-    {
-        "zone": "battery"
-    }
-    */
-
-    char output[256];
-    StaticJsonDocument<256> doc;
-
-    doc["zone"] = "battery";
-
-    // doc.shrinkToFit();  // optional
-
-    serializeJson(doc, output, sizeof(output));
-    if(webSocket.sendTXT(output)) {
-        Serial.println("Register packet sent");
-        // Print the JSON message
-        Serial.println(output);
-        return true;
-    } else {
-        Serial.println("Failed to send register packet");
-        return false;
     }
 }
