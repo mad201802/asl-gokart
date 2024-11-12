884a908663021df2f1b98e5f0ce8d59f32058040
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2024-11-12 18:14:40 +0100

    replaced logic to get package version with `app.getVersion()` call

--- infotainment/headunit/src/helpers/ipc/application/app-listeners.ts
+++ infotainment/headunit/src/helpers/ipc/application/app-listeners.ts
@@ -1,8 +1,8 @@
-import { ipcMain } from "electron";
+import { app, ipcMain } from "electron";
 import { APP_VERSION_CHANNEL } from "./app-channels";
 
 export function addAppEventListeners() {
     ipcMain.handle(APP_VERSION_CHANNEL, async () => {
-        return await process.env.npm_package_version;
+        return await app.getVersion();
     });
 }
