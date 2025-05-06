fe7ffa6d443c58fd22fc189cd66df8d5d3abb9fd
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2025-05-06 18:50:14 +0200

    refactor: update comments in ButtonSlice to clarify button mappings usage

--- infotainment/headunit/src/stores/buttonSlice.ts
+++ infotainment/headunit/src/stores/buttonSlice.ts
@@ -5,6 +5,10 @@ export interface ButtonSlice {
     setButtonMappings: (buttonMappings: ZCButtonStates) => void
 }
 
+/**
+ *  This is not used atm because the button mappings are happening in the backend and only the button testing should be
+ *  in the frontend
+ */
 export const createButtonSlice: StateCreator<
     ButtonSlice,
     [],
