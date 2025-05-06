085d1b08b78c33397bb894606aca43f46942f53e
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2025-05-06 18:56:45 +0200

    removed button slice as there will be only button testing in the UI and the current implementation was to focused on mapping the buttons

--- infotainment/headunit/src/stores/useStore.ts
+++ infotainment/headunit/src/stores/useStore.ts
@@ -4,13 +4,11 @@ import { SystemSlice, createSystemSlice } from './systemSlice'
 import { BatterySlice, createBatterySlice } from './batterySlice'
 import { SharedSlice, createSharedSlice } from './sharedSlice'
 import { createLightsSlice, LightsSlice } from './lightSlice'
-import { createButtonSlice, ButtonSlice } from './buttonSlice'
 
-export const useStore = create<MotorSlice & SystemSlice & BatterySlice & SharedSlice & LightsSlice & ButtonSlice>()((...a) => ({
+export const useStore = create<MotorSlice & SystemSlice & BatterySlice & SharedSlice & LightsSlice>()((...a) => ({
   ...createMotorSlice(...a),
   ...createSystemSlice(...a),
   ...createBatterySlice(...a),
   ...createSharedSlice(...a),
   ...createLightsSlice(...a),
-  ...createButtonSlice(...a)
 }))
