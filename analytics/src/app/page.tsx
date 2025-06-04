8f1dddc6b8275f28bdf63d2f4d90fdee485d10d1
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2025-06-04 23:34:29 +0200

    replaced hello from tRPC API call with fact of the day GET call

--- analytics/src/app/page.tsx
+++ analytics/src/app/page.tsx
@@ -5,7 +5,8 @@ import { auth } from "@/server/auth";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
+  // const hello = await api.post.hello({ text: "from tRPC" });
+  const hello = await api.get.factOfTheDay();
   const session = await auth();
 
   if (session?.user) {
@@ -14,15 +15,15 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#004f6e] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#027ab0] to-[#0c1a2f] text-white">
+        <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16">
           <img src={"/asl-moon-white.png"} />
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            asl-gokart<span className="text-[hsl(209,100%,70%)] font-mono ml-1">/analytics</span>
+            asl-gokart<span className="text-[#42aaff] font-mono ml-1">/analytics</span>
           </h1>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
+            <p className="text-2xl font-light text-white">
+              {hello ? hello : "Loading tRPC query..."}
             </p>
 
             <div className="flex flex-col items-center justify-center gap-4">
@@ -35,6 +36,12 @@ export default async function Home() {
               >
                 {session ? "Sign out" : "Sign in"}
               </Link>
+              {
+                session ?
+                <Link href={"/dashboard"} className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20">
+                  Go to Dashboard
+                </Link> : null
+              }
             </div>
           </div>
 
