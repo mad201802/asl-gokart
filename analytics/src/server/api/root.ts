346a9828d732381bb2bd2d62edd67f281de01fb4
LeoLTM <44781682+LeoLTM@users.noreply.github.com>
2025-06-17 00:44:26 +0200

    feat: add updateRouter to appRouter for handling updates

--- analytics/src/server/api/root.ts
+++ analytics/src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { postRouter } from "@/server/api/routers/post";
 import { getRouter } from "@/server/api/routers/get";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
+import { updateRouter } from "./routers/update";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 export const appRouter = createTRPCRouter({
   post: postRouter,
   get: getRouter,
+  update: updateRouter,
 });
 
 // export type definition of API
