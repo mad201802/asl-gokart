07fbeb40e4f2954bf48bb07812d126c5c5b8f759
Leonard L <44781682+LeoLTM@users.noreply.github.com>
2026-02-28 03:18:14 +0100

    feat: add configurable analytics backend URL and connection checks (#62)

--- infotainment/headunit/src/helpers/ipc/application/app-context.ts
+++ infotainment/headunit/src/helpers/ipc/application/app-context.ts
@@ -1,4 +1,4 @@
-import { APP_VERSION_CHANNEL, APP_TOGGLE_ANALYTICS_CHANNEL } from "./app-channels";
+import { APP_VERSION_CHANNEL, APP_TOGGLE_ANALYTICS_CHANNEL, APP_GET_ANALYTICS_URL_CHANNEL, APP_SET_ANALYTICS_URL_CHANNEL, APP_CHECK_ANALYTICS_CONNECTION_CHANNEL } from "./app-channels";
 
 export function exposeAppContext() {
     const { contextBridge, ipcRenderer } = window.require('electron');
@@ -9,6 +9,15 @@ export function exposeAppContext() {
         },
         toggleAnalytics: async (enabled: boolean): Promise<boolean> => {
             return await ipcRenderer.invoke(APP_TOGGLE_ANALYTICS_CHANNEL, enabled);
+        },
+        getAnalyticsUrl: async (): Promise<string> => {
+            return await ipcRenderer.invoke(APP_GET_ANALYTICS_URL_CHANNEL);
+        },
+        setAnalyticsUrl: async (url: string): Promise<string> => {
+            return await ipcRenderer.invoke(APP_SET_ANALYTICS_URL_CHANNEL, url);
+        },
+        checkAnalyticsConnection: async (url: string): Promise<boolean> => {
+            return await ipcRenderer.invoke(APP_CHECK_ANALYTICS_CONNECTION_CHANNEL, url);
         }
     });
 }
